ca83d56b8c6e2fcfa2d9998020baf011bc23637f
Alexander (Wolf) <qrer71@yandex.ru>
2023-11-04 23:31:22 +0300

    Create models

--- yacut/models.py
+++ yacut/models.py
@@ -1,7 +1,14 @@
 from datetime import datetime
 
-from yacut import db
+from . import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column(db.String(255), nullable=False)
+    short = db.Column(db.String(6), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+
+    def to_dict(self):
+        return dict(url=self.original,
+                    short_link=self.short)
