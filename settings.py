28ff9241cd22514219bc6d37d75a557d8acd846a
Alexander (Wolf) <qrer71@yandex.ru>
2023-11-04 23:31:00 +0300

    Create settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+URL = 'http://localhost/'
+LENGTH = 6
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
