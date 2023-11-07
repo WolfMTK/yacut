e886d6346277e3d897c7c4f5277055751987a38c
Alexander (Wolf) <qrer71@yandex.ru>
2023-11-07 22:27:03 +0300

    Update settings

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@ LENGTH = 6
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI',
+                                        default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
