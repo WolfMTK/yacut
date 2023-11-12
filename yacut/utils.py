08cbdeacc803ae6c9f6892a7b382f9314eee4a78
Alexander (Wolf) <qrer71@yandex.ru>
2023-11-12 15:31:41 +0300

    Update utils

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,14 +1,14 @@
 import random
 from string import ascii_lowercase, digits, ascii_uppercase
 
-from settings import LENGTH
+from settings import LENGTH_MAX
 from .models import URLMap
 
 LETTERS = ascii_lowercase + digits + ascii_uppercase
 
 
 def get_unique_short_id() -> str:
-    return ''.join(random.choice(LETTERS) for i in range(LENGTH))
+    return ''.join(random.choice(LETTERS) for _ in range(LENGTH_MAX))
 
 
 def check_unique_url(short: str) -> bool:
