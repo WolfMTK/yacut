4f9f446571b0e133c43665155e3dc790318dadc8
Alexander (Wolf) <qrer71@yandex.ru>
2023-11-12 15:31:07 +0300

    Update models

--- yacut/models.py
+++ yacut/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 
+from settings import LENGTH_MAX
 from . import db
 
 
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(255), nullable=False)
-    short = db.Column(db.String(6), nullable=False)
+    short = db.Column(db.String(LENGTH_MAX), nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
 
     def to_dict(self):
