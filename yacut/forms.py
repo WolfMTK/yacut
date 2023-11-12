7383c7cfca4bcd95da467de4a8cb0841fe6479a6
Alexander (Wolf) <qrer71@yandex.ru>
2023-11-12 15:30:14 +0300

    Add new validation

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,19 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, URL, Optional
+from wtforms.validators import DataRequired, Length, URL, Optional, Regexp
+
+from settings import LENGTH_MAX, LENGTH_MIN
 
 
 class URLMapForm(FlaskForm):
     original_link = StringField(
         'Длинная ссылка',
-        validators=(DataRequired(message='Обязательное поле'),
-                    Length(1, 255),
+        validators=(DataRequired(message='Обязательное поле.'),
+                    Length(max=255),
                     URL(message='Ошибка ввода URL-адреса.'))
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
         validators=(
-            Length(1, 6, message='Недопустимый размер ссылки.'),
+            Length(min=LENGTH_MIN,
+                   max=LENGTH_MAX,
+                   message='Недопустимый размер ссылки.'),
+            Regexp(r'^[a-zA-Z0-9]*$',
+                   message='Указано недопустимое имя для короткой ссылки.'),
             Optional()
         ))
     submit = SubmitField('Создать')
